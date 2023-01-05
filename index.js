9d09dcb0280804dde3aa63edc9a42716debe5323
AITLASRI <93975504+fatimazahraaitlasri@users.noreply.github.com>
2023-01-05 17:27:44 +0100

    connect project to data base

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express= require('express');
+const colors = require('colors')
 require('dotenv').config();
 const schema =require('./schema/schema')
 const port = process.env.PORT || 5000
 const { graphqlHTTP } = require('express-graphql')
-const app = express()  
-     
+const connectDB = require('./config/config')
+const app = express(); 
+
+
+connectDB()
 
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql : process.env.NODE_ENV === 'development'
 }))
-app.listen(port , console.log(`server runing on port ${port}`))
+app.listen(port , console.log(`server runing on port ${port}`.cyan.underline.bold))
